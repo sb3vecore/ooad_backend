489b59eec6515e1b3f8af738c8eff4fd0b23ab8e
sb3vecore <xebelking@gmail.com>
2024-04-17 12:24:51 +0530

    admin-command-pattern

---
+++ backend/src/main/java/com/project/backend/controller/AdminCommandImplementations/ViewPendingTestsCommand.java
@@ -0,0 +1,14 @@
+package com.project.backend.controller.AdminCommandImplementations;
+
+import com.project.backend.controller.Command;
+import com.project.backend.model.Admin;
+
+public class ViewPendingTestsCommand implements Command {
+
+    public void execute() {
+
+    }
+    public void undo() {
+        
+    } 
+}
